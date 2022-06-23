c7d9e3d6203afb0b355f48bd8d479e46724ed0ad
giurushima <giurushima@hotmail.com>
2022-06-22 22:04:13 -0300

    Employed and Trips added

--- src/components/pages/Employed.js
+++ src/components/pages/Employed.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Employed = () => {
-  return (
-    <div>Employed</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [position, setPosition] = useState("");
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Employed
+  return (
+    <>
+      <h1>Formulario</h1>
+      <form>
+        <label htmlFor="name">NOMBRE/S</label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <label htmlFor="lastName">APELLIDO/S</label>
+        <br />
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <br />
+        <label htmlFor="user">NOMBRE DE USUARIO</label>
+        <br />
+        <input
+          type="text"
+          id="user"
+          name="user"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <br />
+        <label htmlFor="password">CONTRASEÑA</label>
+        <br />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <label htmlFor="position">CARGO/POSICION</label>
+        <br />
+        <select
+          name="position"
+          onChange={(e) => setPosition(e.target.value)}
+          defaultValue=""
+        >
+          <option value="">---</option>
+          <option value="Empleado">Empleado</option>
+          <option value="Camionero">Camionero</option>
+        </select>
+        <br />
+        <button type="submit">CARGAR USUARIO</button>
+      </form>
+    </>
+  );
+};
+
+export default Employed;
--- src/components/pages/Trips.js
+++ src/components/pages/Trips.js
@@ -1,11 +1,69 @@
+import React from "react";
+import { useState } from "react";
 
+const Trips = () => {
+  const [source, setSource] = useState("");
+  const [destiny, setDestiny] = useState("");
+  const [truckdriver, setTruckDriver] = useState("");
+  const [tripstatus, setTripStatus] = useState("");
 
-const trips = () => {
   return (
     <>
-      <p>In</p>
+      <h1>Formulario</h1>
+      <form>
+        <label htmlFor="source">ORIGEN</label>
+        <br />
+        <input
+          type="text"
+          id="source"
+          name="source"
+          value={source}
+          onChange={(e) => setSource(e.target.value)}
+        />
+        <br />
+        <label htmlFor="source">DESTINO</label>
+        <br />
+        <input
+          type="text"
+          id="destiny"
+          name="destiny"
+          value={destiny}
+          onChange={(e) => setDestiny(e.target.value)}
+        />
+        <br />
+        {/* ACA DEBERIAMOS ELEGIR EL NOMBRE DE USUARIO DE CAMIONERO QUE YA ESTAN CARGADOS*/}
+        <label htmlFor="truckdriver">SELECCION DE CAMIONERO</label>
+        <br />
+        <select
+          name="truckdriver"
+          onChange={(e) => setTruckDriver(e.target.value)}
+          defaultValue=""
+        >
+          <option value="">---</option>
+          <option value="">---</option>
+        </select>
+        <br />
+        <label htmlFor="tripstatus">ESTADO DEL VIAJE</label>
+        <br />
+        {/* ACA HAY QUE VER QUE IRIA DEL ESTADO DEL VIAJE 
+        pendiente(cuando se carga en el form, se sube a la api) -
+        confirmado
+        realizado */}
+        <select
+          name="tripstatus"
+          onChange={(e) => setTripStatus(e.target.value)}
+          defaultValue=""
+        >
+          <option value="">---</option>
+          <option value="Pendiente">Pendiente</option>
+          <option value="Confirmado">Confirmado</option>
+          <option value="Realizado">Realizado</option>
+        </select>
+        <br />
+        <button type="submit">CARGAR VIAJE</button>
+      </form>
     </>
   );
 };
 
-export default trips;
+export default Trips;
