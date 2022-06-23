b11b5cebce0784bd2963f0971f9e0331187426af
Feli-Regis <felipeandresregis@gmail.com>
2022-06-22 21:48:02 -0300

    Home and Recepit completed

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,20 @@
+import "./Home.css";
+
 const Home = () => {
-    return (
-      <>
-        <p>In home</p>
-      </>
-    );
-  };
-  
-  export default Home;
-  
+  return (
+    <>
+      <div className="primary">
+        <div className="secondary">
+        <p>Bienvenidos a la pagina Web</p>
+        
+        </div>
+        <h3>Inicio</h3>
+        <h3>Viajes</h3>
+        <h3>Empleados</h3>
+        <h3>Recibos</h3>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- src/components/pages/Receipt.js
+++ src/components/pages/Receipt.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import "./Receipt.css";
 
 const Receipt = () => {
   return (
-    <div>Receipt</div>
-  )
-}
+    <form className="form-receipts">
+      <div className="inputs">
+        <h2>Calculadora de sueldos</h2>
+        <p>Seleccione tipo de usuario:</p>
+        <label>Empleado</label>
+        <input type="radio" name="1" />
+        <label>Camionero</label>
+        <input type="radio" name="1" />
+        <br />
+        <label>Ingrese cantidad de horas trabajadas:</label>
+        <input type="text" />
+        <br />
+        <button type="submit" className="btn">Calcular costo</button>
+      </div>
+    </form>
+  );
+};
 
-export default Receipt
+export default Receipt;
